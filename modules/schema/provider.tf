09813a7ae1bb0af4dcd6d9899263290ff41419ff
Bugsy <gsubrama94@outlook.com>
2026-04-30 21:07:42 -0400

    All provider changed to Trial Version

--- modules/schema/provider.tf
+++ modules/schema/provider.tf
@@ -9,10 +9,11 @@ terraform {
   }
 }
 provider "snowflake" {
- organization_name = "MFSG"
- account_name = "MONEYMART_DEV"
- user = "SVC_TERRAFORM"
- private_key = file("rsa_key.p8")
- authenticator = "SNOWFLAKE_JWT"
- role = "MFSG_TERRAFORM_ADMIN"
+  organization_name = "SPYPLHP"
+  account_name      = "VU59987"
+  user              = "GSUBRAMA"
+  private_key       = file("rsa_key.p8")
+  authenticator     = "SNOWFLAKE_JWT"
+  role              = "ACCOUNTADMIN"
+  preview_features_enabled = ["snowflake_table_resource"]
 }
